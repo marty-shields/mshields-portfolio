3a1dce38a718dad4f8f34a802f6cbe3dd9c70a7e
Martin <martinshields@hotmail.com>
2017-08-15 19:26:31 +0100

    uni route
    updated uni page to incorporate projects. now loading to new blank page

--- routes/uni.js
+++ routes/uni.js
@@ -14,26 +14,48 @@ var uri = "mongodb://marty-shields:1loD8ziYiusb8A1r@cluster0-shard-00-00-" +
 
 
 /* --------------------------------ROUTES----------------------------------- */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   MongoClient.connect(uri, function(err, db) {
     if (err) {
       res.status(500).send('Something broke!');
     }
+
+    db.collection("uni-work").deleteMany(function(err, obj) {
+      if (err) throw err;
+      console.log(obj.result.n + " document(s) deleted");
+      db.close();
+    });
+
+    db.collection('uni-work').insertOne({
+      title : "Database Administration Tool - Dissertation",
+      img : "https://img.youtube.com/vi/FYvno44PK-Y/mqdefault.jpg",
+      overview : {
+        summary : "Database Administration and Backup Tool",
+        languages : "HTML, CSS, JS, JQuery, Node.JS",
+        frameworks : "Bootstrap, Chart.JS",
+        github : "Insert Link Here"
+      }
+    });
 
     db.collection('uni-work').find({}).toArray(function (err, query){
       if (err) throw err;
 
       db.close();
 
-      console.log(query);
+      var results = {projects : query};
+      console.log(results);
 
       res.render(path.join('uni'), {
         title: 'MS Portfolio - Uni',
-        item: query
+        item: results
       });
     });
   });
 
+});
+
+router.get('/:id', function(req, res) {
+  res.send('show page!');
 });
 
 //db.collection('uni-work').insertOne({
@@ -41,6 +63,7 @@ router.get('/', function(req, res, next) {
 //  img : "https://img.youtube.com/vi/FYvno44PK-Y/mqdefault.jpg",
 //  overview : {
 //    summary : "Database Administration and Backup Tool",
+//    route: "uni/dissertation"
 //    languages : "HTML, CSS, JS, JQuery, Node.JS",
 //    frameworks : "Bootstrap, Chart.JS",
 //    github : "Insert Link Here"
