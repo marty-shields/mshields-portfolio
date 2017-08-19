2bc9e378e5c1de9c4630bd1d65148a3ba0b9babe
Martin <martinshields@hotmail.com>
2017-08-19 20:57:16 +0100

    .
    .

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/uni', uni);
+app.use("/da", express.static(__dirname + '/da'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
