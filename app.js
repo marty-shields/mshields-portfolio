4570dccf4e747b9133b16e8b08ac2abf3eda8984
Martin <martinshields@hotmail.com>
2017-08-20 07:47:19 +0100

    .
    .

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.set('da', express.static(path.join(__dirname, 'da')));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -29,10 +30,10 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('da', express.static(path.join(__dirname, 'da')));
 
 app.use('/', index);
 app.use('/uni', uni);
+app.use(mongo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
