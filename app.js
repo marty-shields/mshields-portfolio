11545f9b4787fb2ac6c279e33eb8ac2d2156502b
Martin <martinshields@hotmail.com>
2017-08-20 08:25:44 +0100

    .
    .

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.set('da', (path.join(__dirname, 'data-access')));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
