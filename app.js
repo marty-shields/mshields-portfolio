79544eef4018507333e65063b7cecbb9804b945e
Martin <martinshields@hotmail.com>
2017-08-20 07:58:09 +0100

    .
    .

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/uni', uni);
 app.use('/', express.static(path.join(__dirname, 'da')));
+app.use('/MongoDbConnector', mongo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
