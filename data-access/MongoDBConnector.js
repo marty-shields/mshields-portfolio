1e648b08dce2b104c79795f252c13a0027ad16b5
Martin <martinshields@hotmail.com>
2017-08-28 16:20:12 +0100

    config
    added config and put personal stuff into it

--- data-access/MongoDBConnector.js
+++ data-access/MongoDBConnector.js
@@ -1,14 +1,11 @@
 /*jshint esversion: 6 */
 "use strict";
 var mongo = require('mongodb');
+const config = require("../config.json");
 
 module.exports = class MongoDbConnector {
   constructor(){
-    this.uri = "mongodb://marty-shields:1loD8ziYiusb8A1r@cluster0-shard-00-00-" +
-    "uuh61.mongodb.net:27017,cluster0-shard-00-01-" +
-    "uuh61.mongodb.net:27017,cluster0-shard-00-02-" +
-    "uuh61.mongodb.net:27017/portfolio?ssl=true&replicaSet=Cluster0-" +
-    "shard-0&authSource=admin";
+    this.uri = config.connectionStrings.dev;
   }
 
   AllUniWorkRecords(coll){
